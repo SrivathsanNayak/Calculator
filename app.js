60ddf3f315c526ff460ab320b03b7f643e8dc656
SrivathsanNayak <ganeshsrivathsan@gmail.com>
2021-07-27 13:49:29 +0530

    Added operate function

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
-function add(a, b) {
+function add(a,b) {
     return (a+b);
 }
 
-function subtract(a, b) {
+function subtract(a,b) {
     return (a-b);
 }
 
-function multiply(a, b) {
+function multiply(a,b) {
     return (a*b);
 }
 
-function divide(a, b) {
+function divide(a,b) {
     return ((b !== 0) ? (a/b) : "ERROR!");
 }
 
-console.log(add(4,5));
-console.log(subtract(80.3,3));
-console.log(multiply(3.5,0));
-console.log(divide(100,13));
+function operate(operator,a,b) {
+    switch(operator) {
+        case "+":
+            console.log(add(a,b));
+            break;
+        case "-":
+            console.log(subtract(a,b));
+            break;
+        case "*":
+            console.log(multiply(a,b));
+            break;
+        case "/":
+            console.log(divide(a,b));
+            break;
+        default:
+            console.log("Invalid!");
+    }
+}
+
+operate("+",2,3);
+operate("-",3,4);
+operate("*",9.5,7);
+operate("/",4,99);
