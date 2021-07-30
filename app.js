66dec586e16a61d31bf2fff78525965b4a7132e7
SrivathsanNayak <ganeshsrivathsan@gmail.com>
2021-07-30 22:49:08 +0530

    Deleted comments containing old code

--- app.js
+++ app.js
@@ -55,56 +55,6 @@ function displayOperator() {
     }
 }
 
-/*
-let displayValue = 0;
-let firstValue = 0;
-let operatorUsed = "";
-let secondValue = 0;
-let resultValue = 0;
-let operatorAdded = false;
-
-numbers.forEach(num => num.addEventListener("click", () => {
-    if (divDisplay.textContent.length < 12) {
-        if (!operatorAdded) {
-            if (divDisplay.textContent.match(/^(0)\1*<insert forwardslash>)) {
-                divDisplay.textContent = num.textContent;
-            }
-            else {
-                divDisplay.textContent += num.textContent;
-            }
-        } else {
-            if (divDisplay.textContent.match(/^(0)\1*<insert forwardslash>)) {
-                divDisplay.textContent = num.textContent;
-            }
-            else {
-                divDisplay.textContent += num.textContent;
-            }
-        }
-    }
-    displayValue = parseInt(divDisplay.textContent);
-    //console.log(`${displayValue}: ${typeof(displayValue)}`);
-}));
-
-operators.forEach(operator => operator.addEventListener("click", () => {
-    if (!operatorAdded) {
-        firstValue = displayValue;
-        operatorUsed = operator.textContent;
-        operatorAdded = true;
-        console.log(`Display value: ${displayValue}, Operator: ${operatorUsed}, First value: ${firstValue}`);
-        divDisplay.textContent = "0";
-    } else {
-        secondValue = displayValue;
-        console.log(`Display value: ${displayValue}, Operator: ${operatorUsed}, Second value: ${secondValue}`);
-        operate(operatorUsed, firstValue, secondValue);
-    }
-}));
-
-divClear.addEventListener("click", () => {
-    divDisplay.textContent = "0";
-    displayValue = 0;
-});
-*/
-
 function add(a, b) {
     return (a + b);
 }
